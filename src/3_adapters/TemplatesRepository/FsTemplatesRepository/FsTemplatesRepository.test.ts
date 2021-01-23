1ab510256edb57511a489685f608a6edeb67bb59
streletss <xstreletssx@gmail.com>
2021-01-23 22:57:48 +0200

    update templates repo implementation

--- src/3_adapters/TemplatesRepository/FsTemplatesRepository/FsTemplatesRepository.test.ts
+++ src/3_adapters/TemplatesRepository/FsTemplatesRepository/FsTemplatesRepository.test.ts
@@ -1,13 +1,41 @@
+import path from "path";
 import TemplatesRepository from "./FsTemplatesRepository";
 
-describe.only("FsTemplatesRepository", () => {
-  it("should find template by name", () => {
-    const templatesPath = "../../../../__tests__/fixtures/templates";
-    const repo = new TemplatesRepository(templatesPath);
-    const template = repo.getTemplateByName("fake");
+describe.skip("FsTemplatesRepository", () => {
+  let templatesRepo: TemplatesRepository;
 
-    expect(template).toEqual({
-      path: undefined,
+  beforeAll(() => {
+    const repoTemplatesPath = "../../../../__tests__/fixtures/templates";
+    const projectTemplatesPath = "./.bystencil";
+    const projectPath = path.resolve(
+      __dirname,
+      "../../../../__tests__/fixtures/project",
+    );
+
+    templatesRepo = new TemplatesRepository(
+      repoTemplatesPath,
+      projectPath,
+      projectTemplatesPath,
+    );
+  });
+
+  it("should find project based template by name", () => {
+    const template = templatesRepo.getTemplateByName("project_based");
+
+    expect(template).toMatchObject({
+      files: [
+        {
+          path: "__DisplayName__.tsx",
+          contents: "// some project template file\n",
+        },
+      ],
+    });
+  });
+
+  it("should find repo based template by name", () => {
+    const template = templatesRepo.getTemplateByName("repo_based");
+
+    expect(template).toMatchObject({
       files: [
         {
           path: "__DisplayName__.stories.mdx",
@@ -22,6 +50,19 @@ describe.only("FsTemplatesRepository", () => {
         {
           path: "styles/__DisplayName__.module.scss",
           contents: ".__DisplayName__ {\n  display: none;\n}\n",
+        },
+      ],
+    });
+  });
+
+  it("should find project based templates first", () => {
+    const template = templatesRepo.getTemplateByName("conflicting");
+
+    expect(template).toMatchObject({
+      files: [
+        {
+          path: "__DisplayName__.tsx",
+          contents: "// project based conflicting template file",
         },
       ],
     });
--- src/3_adapters/TemplatesRepository/FsTemplatesRepository/FsTemplatesRepository.ts
+++ src/3_adapters/TemplatesRepository/FsTemplatesRepository/FsTemplatesRepository.ts
@@ -4,25 +4,39 @@ import glob from "glob";
 import { TemplatesRepositoryInstance } from "../../../2_usecases/interfaces";
 
 class TemplatesRepository implements TemplatesRepositoryInstance {
-  constructor(private templatesPath: string = "../../../templates") {}
+  constructor(
+    private repoTemplatesPath: string,
+    private projectPath: string,
+    private projectTemplatesPath: string,
+  ) {}
 
   public getTemplateByName = (name: string) => {
-    const templatePath = path.join(__dirname, this.templatesPath, name);
+    const templatePath = [
+      this.getProjectBasedTemplatePath(name),
+      this.getRepoBasedTemplatePath(name),
+    ].find(fs.existsSync);
 
-    if (!fs.existsSync(templatePath)) return null;
-
-    const files = glob
-      .sync(`${templatePath}/**/*`, { nodir: true })
-      .map((file) => {
-        const contents = fs.readFileSync(file).toString();
-        const filepath = path.relative(templatePath, file);
-        return { path: filepath, contents };
-      });
+    if (!templatePath) return null;
 
     return {
-      path: undefined,
-      files,
+      files: this.getTemplateFiles(templatePath),
     };
+  };
+
+  private getRepoBasedTemplatePath = (templateName: string) => {
+    return path.join(__dirname, this.repoTemplatesPath, templateName);
+  };
+
+  private getProjectBasedTemplatePath = (templateName: string) => {
+    return path.join(this.projectPath, this.projectTemplatesPath, templateName);
+  };
+
+  private getTemplateFiles = (templatePath: string) => {
+    return glob.sync(`${templatePath}/**/*`, { nodir: true }).map((file) => {
+      const contents = fs.readFileSync(file).toString();
+      const filepath = path.relative(templatePath, file);
+      return { path: filepath, contents };
+    });
   };
 }
 
