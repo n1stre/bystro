69ee23153303c294cc1b199a19f840638eb038d7
streletss <xstreletssx@gmail.com>
2021-01-26 13:33:04 +0200

    add templates controller adapter

---
+++ src/3_adapters/TemplatesController/index.ts
@@ -0,0 +1,25 @@
+import path from "path";
+import CloneTemplateIntoPath from "../../2_usecases/CloneTemplateIntoPath";
+import ListTemplateVariables from "../../2_usecases/ListTemplateVariables";
+import TemplatesRepository from "../TemplatesRepository";
+import FileSystemAdapter from "../FileSystemAdapter";
+
+const filesystem = FileSystemAdapter.make();
+const templatesRepository = TemplatesRepository.make(
+  path.resolve(__dirname, "../../../templates"),
+  path.resolve(process.cwd(), ".bystencil"),
+  [".templaterc"],
+);
+
+const TemplatesController = Object.freeze({
+  cloneIntoPath: CloneTemplateIntoPath.build({
+    templatesRepository,
+    filesystem,
+  }).exec,
+
+  listVariables: ListTemplateVariables.build({
+    templatesRepository,
+  }).exec,
+});
+
+export default TemplatesController;
