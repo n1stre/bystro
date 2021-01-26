8185877da4516ef1edc50652ed1cd1f04e564030
streletss <xstreletssx@gmail.com>
2021-01-26 13:33:20 +0200

    move main and bin to index.ts

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import parseArgs from "minimist";
+import inquirer from "inquirer";
+import chalk from "chalk";
+import TemplatesController from "./3_adapters/TemplatesController";
+
+run().catch((err) => {
+  console.log(chalk.red(err.message));
+  process.exit(err);
+});
+
+async function run() {
+  const args = parseArgs(process.argv.slice(2));
+  const templateName = args._[0];
+  const templatePath = args._[1];
+
+  const vars = await TemplatesController.listVariables({ name: templateName });
+
+  const answers = await inquirer.prompt(
+    vars.map((v) => ({
+      name: v.name,
+      message: `Enter ${v.name} (${v.description}):`,
+      type: "input",
+    })),
+  );
+
+  await TemplatesController.cloneIntoPath({
+    path: templatePath,
+    name: templateName,
+    variables: answers,
+  });
+
+  console.log(
+    chalk.green(`Successfully cloned "${templateName}" into ${templatePath}`),
+  );
+}
