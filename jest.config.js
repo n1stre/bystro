99c5ab4695738d0d2778b8bc82768e9e4879d47e
streletss <xstreletssx@gmail.com>
2021-01-28 18:23:26 +0200

    ignore coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -13,12 +13,12 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts*"],
   coverageDirectory: "test/coverage",
   coveragePathIgnorePatterns: ["/node_modules/"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
 };
